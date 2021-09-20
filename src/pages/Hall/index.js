42185f79d7ad7aa5b696a3e8d0e62b8fe039c56c
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-09-20 20:13:56 -0300

    Inserindo hora e data atual no salão

--- src/pages/Hall/index.js
+++ src/pages/Hall/index.js
@@ -213,13 +213,40 @@ export default function Hall() {
 			});
 	}, []);
 	console.log(allProducts)
+
+	const newDate = new Date();
+	const dayName = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta", "Sábado"];
+	const week = dayName[newDate.getDay() ]
+	const day = String(newDate.getDate()).padStart(2, '0');
+	const monthName = ["jan,", "fev,", "mar.", "abr.", "maio", "jun.", "jul.", "ago.", "set.", "out.", "nov.", "dez."];
+	const month = monthName[newDate.getMonth()];
+	const year = newDate.getFullYear();
+
+	const [currentHour, serCurrentHour] = useState(new Date());
+
+	function refreshClock() {
+		serCurrentHour(new Date());
+	}
+	useEffect(() => {
+		const timerId = setInterval(refreshClock, 1000);
+		return function cleanup() {
+			clearInterval(timerId);
+		};
+	}, []);
 	
 	return (
 		<div className="pages-container">
 			<header className="title-area">
 				<div>
 					<h1 className="uppercase">Divino Burger</h1>
-					<p>horário atual</p>
+					<p>
+						{day} {month} {year}, {week} - 🕐
+						{currentHour.toLocaleTimeString('pt-br', {
+							hour: '2-digit',
+							minute: '2-digit',
+						})}
+						h
+					</p>
 				</div>
 
 				<ButtonLogout />
